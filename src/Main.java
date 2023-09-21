9ca4d7e1eb9b807f2601dbe2feae590540d51682
Ulia <cs-gorodok@yandex.ru>
2023-09-21 21:51:10 +0300

    change src

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int rub = 100;// остаток у клиента
+        int rub sum1 = 300;// сумма пополнения
+        int rub sum2 = 1100;// сумма пополнения
+
+        System.out.println((rub + rub sum1) + " итоговая сумма 1");
+        System.out.println((rub + rub sum2) + " итоговая сумма 2");
+
+    }
+}
