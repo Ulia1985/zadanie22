3ce54aa11695922880d4fcb8c7830704545b576e
Ulia <cs-gorodok@yandex.ru>
2023-10-01 18:11:52 +0300

    change src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,19 @@ public class Main {
     public static void main(String[] args) {
 
         int rub = 100;// остаток у клиента
-        int sum1 = 300;// сумма пополнения 20.09.2023г.
-        int sum2 = 1100;// сумма пополнения 21.09.2023г.
+        int sum = 1300;// сумма пополнения
 
-        System.out.println((rub + sum1) + " руб. итоговая сумма 20.09.2023г.");
-        System.out.println((rub + sum2) + " руб. итоговая сумма 21.09.2023г.");
+        System.out.println((rub + sum) + " руб. итоговая сумма");
 
         int ball1 = 0;
-        if (sum1 > 1000) {
-            ball1 = sum1 / 100;
+        if (sum > 1000) {
+            ball1 = sum / 100;
         }
 
-        int ball2 = 0;
-        if (sum2 > 1000) {
-            ball2= sum2 / 100;
-        }
+        int fin1 =rub + sum + ball1;
 
-        int fin1 =rub + sum1 + ball1;
-        int fin2 =rub + sum2 + ball2;
-        System.out.println( fin1 +" руб. итоговая сумма с бонусами 20.09.2023г.");
-        System.out.println( fin2 +" руб. итоговая сумма с бонусами 21.09.2023г.");
-        System.out.println(ball1 + " бонусов 20.09.2023г.");
-        System.out.println(ball2 + " бонусов 21.09.2023г.");
+        System.out.println( fin1 +" руб. итоговая сумма с бонусами");
+        System.out.println(ball1 + " бонусов");
 
     }
 }
